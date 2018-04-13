38087f2dacc1226943dbd9d782e84089b418799a
Justine <justine.probst@munichmotorsport.de>
2018-04-13 14:00:46 +0200

    Added TCP sender functionality, not tested

--- library/receiver.h
+++ library/receiver.h
@@ -15,7 +15,7 @@
 #ifndef RECEIVER_H
 #define RECEIVER_H
 
-// udp includes
+// udp/tcp includes
 #include <arpa/inet.h>
 #include <iostream>
 #include <memory>
@@ -47,220 +47,209 @@ namespace connector {
      * See examples at [tests/receiver_tests.cc](../../tests/receiver_tests.cc)
      */
     template<CONNECTOR_TYPE T> // T = object-to-receive
-    class receiver
-    { };
+        class receiver
+        { };
 
     template<>
-    class receiver<TCP>
-    {
-      public:
-          //! TCP constructor, client_count is default on 1
-          receiver (const int port, const int client_count = 1)
-          : _port(port)
-          , _client_count(client_count)
-          { }
-
-      // methods
-      public:
-
-        int init(){
-          // creating socket
-          _listen_socket = socket(AF_INET, SOCK_STREAM, 0);
-          if(_listen_socket == -1)
-          {
-              DEBUG_MSG("Error creating socket with -1");
-              return -1;
-          } else
-          {
-              DEBUG_MSG("Created socket!");
-          }
-
-          // binding port
-          _servAddr.sin_family = AF_INET;
-          _servAddr.sin_addr.s_addr = INADDR_ANY;
-          _servAddr.sin_port = htons(_port);
-          _bnd = bind(_listen_socket,(struct sockaddr *) &_servAddr, sizeof(_servAddr));
-          if(_bnd < 0)
-          {
-              DEBUG_MSG("Error binding with error: " << _bnd);
-              return -1;
-          } else
-          {
-              DEBUG_MSG("Binding successful to port " << _port << "!");
-          }
-
-          // waiting for _skt clients to connect (blocking)
-          listen(_listen_socket, _client_count);
-          _len = sizeof(struct sockaddr_in);
-          _skt = accept(_listen_socket, (struct sockaddr *) &_cliAddr, (socklen_t*) &_len);
-          if(_skt < 0)
-          {
-            DEBUG_MSG("Connection accept failed with error: " << _skt);
-            return -1;
-          } else
-          {
-            DEBUG_MSG("All clients (" << _client_count << ") connected!");
-          }
-          return _skt;
-        }
-
-        //! \todo
-        template<class U>
-        void receive_tcp(U & out) const
+        class receiver<TCP>
         {
-          recv(_skt, static_cast<void*>(&out), sizeof(U), 0);
-        }
+            public:
+                //! TCP constructor, client_count is default on 1
+                receiver (const int port, const int client_count = 1)
+                    : _port(port), 
+                      _client_count(client_count)
+            { }
 
-        //! \todo
-        template<class U>
-        void receive_tcp(U & out, const size_t buffer) const
-        {
-          recv(_skt, static_cast<void*>(&out), buffer, 0);
-        }
+                // methods
+            public:
 
-      // member
-      private:
-        /** \brief A container to store sender information
-         *
-         * Members:
-         *  * `short` sin_family
-         *  * `unsigned short` sin_port
-         *  * `struct` in_addr sin_addr
-         *  * `char` sin_zero[0]
-         *
-         * Needed include: `<netinet/in.h>`
-         */
-        struct sockaddr_in _servAddr;
-        /** \brief A container to store client information
-         *
-         * Members:
-         *  * `short` sin_family
-         *  * `unsigned short` sin_port
-         *  * `struct` in_addr sin_addr
-         *  * `char` sin_zero[0]
-         *
-         * Needed include: `<netinet/in.h>`
-         */
-        struct sockaddr_in _cliAddr;
-        /** \brief The socket to send data to the client from
-        */
-        int _skt{0};
-        /** \brief Return value of `bind()`
-        */
-        int _bnd{0}; 
-        /** \brief Length of IP-Adress
-        */
-        unsigned _len{0}; // Length of ipAdress
-        int _listen_socket;
+                int init(){
+                    // creating socket
+                    _listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+                    if(_listen_socket == -1)
+                    {
+                        DEBUG_MSG("Error creating socket with -1");
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("Created socket!");
+                    }
 
-        //! \todo
-        int _port;
-        //! \todo
-        int _client_count;
+                    // binding port
+                    _servAddr.sin_family = AF_INET;
+                    _servAddr.sin_addr.s_addr = INADDR_ANY;
+                    _servAddr.sin_port = htons(_port);
+                    _bnd = bind(_listen_socket,(struct sockaddr *) &_servAddr, sizeof(_servAddr));
+                    if(_bnd < 0)
+                    {
+                        DEBUG_MSG("Error binding with error: " << _bnd);
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("Binding successful to port " << _port << "!");
+                    }
 
-    };
+                    // waiting for _skt clients to connect (blocking)
+                    listen(_listen_socket, _client_count);
+                    _len = sizeof(struct sockaddr_in);
+                    _skt = accept(_listen_socket, (struct sockaddr *) &_cliAddr, (socklen_t*) &_len);
+                    if(_skt < 0)
+                    {
+                        DEBUG_MSG("Connection accept failed with error: " << _skt);
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("All clients (" << _client_count << ") connected!");
+                    }
+                    return _skt;
+                }
+
+                //! \todo
+                template<class U>
+                    void receive_tcp(U & out) const
+                    {
+                        recv(_skt, static_cast<void*>(&out), sizeof(U), 0);
+                    }
+
+                //! \todo
+                template<class U>
+                    void receive_tcp(U & out, const size_t buffer) const
+                    {
+                        recv(_skt, static_cast<void*>(&out), buffer, 0);
+                    }
+
+                // member
+            private:
+                /** \brief A container to store sender information
+                 *
+                 * Members:
+                 *  * `short` sin_family
+                 *  * `unsigned short` sin_port
+                 *  * `struct` in_addr sin_addr
+                 *  * `char` sin_zero[0]
+                 *
+                 * Needed include: `<netinet/in.h>`
+                 */
+                struct sockaddr_in _servAddr;
+                /** \brief A container to store client information
+                 *
+                 * Members:
+                 *  * `short` sin_family
+                 *  * `unsigned short` sin_port
+                 *  * `struct` in_addr sin_addr
+                 *  * `char` sin_zero[0]
+                 *
+                 * Needed include: `<netinet/in.h>`
+                 */
+                struct sockaddr_in _cliAddr;
+                //! The socket to send data to the client from
+                int _skt{0};
+                //! Return value of `bind()`
+                int _bnd{0}; 
+                //! Length of IP-Adress
+                unsigned _len{0}; // Length of ipAdress
+                int _listen_socket;
+                //! \todo
+                int _port;
+                //! \todo
+                int _client_count;
+
+        };
 
 
     template<>
-    class receiver<UDP>
-    {
-      public:
-          //! UDP constructor
-          receiver (const int port)
-          : _port(port)
-          { }
-
-      // methods
-      public:
-
-        int init(){
-          // bind socket to local server port
-          _servAddr.sin_family = AF_INET;
-          _servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-          _servAddr.sin_port = htons(_port);
-
-          // create a UDP socket
-          _skt = socket(AF_INET, SOCK_DGRAM, 0);
-          if(_skt < 0)
-          {
-            DEBUG_MSG("Connection accept failed with error: " << _skt);
-            return -1;
-          } else
-          {
-            DEBUG_MSG("Some clients connected!");
-          }
-
-          // binding socket
-          _bnd = bind(_skt, (struct sockaddr *) &_servAddr, sizeof(struct sockaddr_in));
-          if(_bnd < 0)
-          {
-              DEBUG_MSG("Error binding with error: " << _bnd);
-              return -1;
-          } else
-          {
-              DEBUG_MSG("Binding successful to port " << _port << "!");
-          }
-          _len = sizeof(struct sockaddr_in); // needed to store length of the IP-Adress
-          return _skt;
-        }
-
-        /** \brief Main UDP function to receive objects
-         *
-         * Modifying:
-         *    * `out`
-         */
-        template<class U>
-        void receive_udp(U & out) const
+        class receiver<UDP>
         {
-            recvfrom(_skt, &out, sizeof(U), 0, (struct sockaddr *) &_cliAddr, &_len); 
-        }
+            public:
+                //! UDP constructor
+                receiver (const int port)
+                    : _port(port)
+                { }
 
-        template<class U>
-        void receive_udp(U & out, const size_t buffer)
-        {
-            recvfrom(_skt, static_cast<void*>(&out), buffer, 0, (struct sockaddr *) &_cliAddr, &_len); 
-        }
+                // methods
+            public:
 
-      // member
-      private:
-        /** \brief A container to store sender information
-         *
-         * Members:
-         *  * `short` sin_family
-         *  * `unsigned short` sin_port
-         *  * `struct` in_addr sin_addr
-         *  * `char` sin_zero[0]
-         *
-         * Needed include: `<netinet/in.h>`
-         */
-        struct sockaddr_in _servAddr;
+                int init(){
+                    // bind socket to local server port
+                    _servAddr.sin_family = AF_INET;
+                    _servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+                    _servAddr.sin_port = htons(_port);
 
-        /** \brief A container to store client information
-         *
-         * Members:
-         *  * `short` sin_family
-         *  * `unsigned short` sin_port
-         *  * `struct` in_addr sin_addr
-         *  * `char` sin_zero[0]
-         *
-         * Needed include: `<netinet/in.h>`
-         */
-        struct sockaddr_in _cliAddr;
-        /** \brief The socket to send data to the client from
-        */
-        int _skt{0};
-        /** \brief Return value of `bind()`
-        */
-        int _bnd{0}; 
-        /** \brief Length of IP-Adress
-        */
-        unsigned _len{0}; // Length of ipAdress
+                    // create a UDP socket
+                    _skt = socket(AF_INET, SOCK_DGRAM, 0);
+                    if(_skt < 0)
+                    {
+                        DEBUG_MSG("Connection accept failed with error: " << _skt);
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("Some clients connected!");
+                    }
 
-        //! \todo
-        int _port;
+                    // binding socket
+                    _bnd = bind(_skt, (struct sockaddr *) &_servAddr, sizeof(struct sockaddr_in));
+                    if(_bnd < 0)
+                    {
+                        DEBUG_MSG("Error binding with error: " << _bnd);
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("Binding successful to port " << _port << "!");
+                    }
+                    _len = sizeof(struct sockaddr_in); // needed to store length of the IP-Adress
+                    return _skt;
+                }
 
-    };
+                /** \brief Main UDP function to receive objects
+                 *
+                 * Modifying:
+                 *    * `out`
+                 */
+                template<class U>
+                    void receive_udp(U & out) const
+                    {
+                        recvfrom(_skt, &out, sizeof(U), 0, (struct sockaddr *) &_cliAddr, &_len); 
+                    }
 
+                template<class U>
+                    void receive_udp(U & out, const size_t buffer)
+                    {
+                        recvfrom(_skt, static_cast<void*>(&out), buffer, 0, (struct sockaddr *) &_cliAddr, &_len); 
+                    }
 
+                // member
+            private:
+                /** \brief A container to store sender information
+                 *
+                 * Members:
+                 *  * `short` sin_family
+                 *  * `unsigned short` sin_port
+                 *  * `struct` in_addr sin_addr
+                 *  * `char` sin_zero[0]
+                 *
+                 * Needed include: `<netinet/in.h>`
+                 */
+                struct sockaddr_in _servAddr;
+
+                /** \brief A container to store client information
+                 *
+                 * Members:
+                 *  * `short` sin_family
+                 *  * `unsigned short` sin_port
+                 *  * `struct` in_addr sin_addr
+                 *  * `char` sin_zero[0]
+                 *
+                 * Needed include: `<netinet/in.h>`
+                 */
+                struct sockaddr_in _cliAddr;
+                //! The socket to send data to the client from
+                int _skt{0};
+                //! Return value of `bind()`
+                int _bnd{0}; 
+                //! Length of IP-Adress
+                unsigned _len{0}; 
+                //! \todo
+                int _port;
+        };
 } // namespace connector
 #endif // RECEIVER_H
--- library/sender.h
+++ library/sender.h
@@ -15,7 +15,7 @@
 #ifndef SENDER_H
 #define SENDER_H
 
-// udp includes
+//udp/tcp includes
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -30,61 +30,179 @@
  */
 namespace connector {
 
-/** \brief A templated Sender class with one template argument
- *
- * Template argument:
- * * `T` = Type of the object-to-send
- *
- * See examples at [tests/sender_tests.cc](../../tests/sender_tests.cc)
- */
-template<typename T> // T = object-to-send
-class sender {
+    //! \todo
+    enum CONNECTOR_TYPE : size_t
+    { 
+        TCP = 0, UDP = 1
+    };
+    /** \brief A templated Sender class with one template argument
+     *
+     * Template argument:
+     * * `T` = Type of the object-to-send
+     *
+     * See examples at [tests/sender_tests.cc](../../tests/sender_tests.cc)
+     */
+    template<CONNECTOR_TYPE T> // T = object-to-send
+        class sender 
+        { };
 
-    // methods
-    public:
-      /**
-       * This function initializes the parameters needed for establishing a **UDP** connection with the receiver
-       * It takes a port (`int`) and an IP Adress(`std::string`) as Arguments and
-       * only needs to be called once
-       *
-       * Modifying:
-       *    * `_cliAddr`
-       *    * `_skt`
-       *    * `port`
-       *    * `ip`
-       */
-      void init_sender_udp(const int port, const std::string ip) {
-        _skt = socket(AF_INET, SOCK_DGRAM, 0);
-        _cliAddr.sin_family = AF_INET;
-        _cliAddr.sin_port = htons(port);
-        inet_pton(AF_INET, ip.c_str(), &(_cliAddr.sin_addr));
-      }
+    template<> 
+        class sender<TCP>
+        {
 
-      /** \brief Main UDP function to send objects
-       * 
-       * The `object_to_send` should be of the same type and size (`buffer`) as the object on the [receiving](classconnector_1_1receiver.html) side
-       */
-        void send_udp(T * object_to_send, size_t buffer=2048) {
+            public:
+                //! TCP constructor, client_count is default on 1
+                sender (const int port, const std::string ip) 
+                    : _port(port), 
+                    _ip(ip) 
+            { }
 
-          sendto(_skt, object_to_send, buffer, 0, (struct sockaddr *) &_cliAddr, sizeof(sockaddr_in));
+                // methods
+            public:
+                /**
+                 *! \todo 
+                 * Modifying:
+                 *    * `_cliAddr`
+                 *    * `_skt`
+                 *    * `_ip`
+                 *    * `_port`
+                 */
+                void init() {
+                    // creating socket
+                    _listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+                    if(_listen_socket == -1)
+                    {
+                        DEBUG_MSG("Error creating socket with -1");
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("Created socket!");
+                    }
+
+                    // binding port
+                    _servAddr.sin_family = AF_INET;
+                    _servAddr.sin_addr.s_addr = INADDR_ANY;
+                    _servAddr.sin_port = htons(_port);
+                    _bnd = bind(_listen_socket,(struct sockaddr *) &_servAddr, sizeof(_servAddr));
+                    if(_bnd < 0)
+                    {
+                        DEBUG_MSG("Error binding with error: " << _bnd);
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("Binding successful to port " << _port << "!");
+                    }
+
+                    // waiting for _skt clients to connect (blocking)
+                    listen(_listen_socket, _client_count);
+                    _len = sizeof(struct sockaddr_in);
+                    _skt = accept(_listen_socket, (struct sockaddr *) &_cliAddr, (socklen_t*) &_len);
+                    if(_skt < 0)
+                    {
+                        DEBUG_MSG("Connection accept failed with error: " << _skt);
+                        return -1;
+                    } else
+                    {
+                        DEBUG_MSG("All clients (" << _client_count << ") connected!");
+                    }
+                    return _skt;
+                }
+
+        }
+    //! \todo
+    template<class U>
+        void send_tcp(U & out) const
+        {
+            send(_skt, &out, sizeof(U), 0);
+        }
+
+    //! \todo
+    template<class U>
+        void send_tcp(U & out, const size_t buffer) const
+        {
+            send(_skt, static_cast<void*>(&out), buffer, 0);
         }
 
     // member
     private:
-        /** \brief A container to store client information
-         *
-         * Members:
-         *  * `short` sin_family
-         *  * `unsigned short` sin_port
-         *  * `struct` in_addr sin_addr
-         *  * `char` sin_zero[0]
-         *
-         * Needed include: `<netinet/in.h>`
-         */
-          struct sockaddr_in _cliAddr;
-          /** \brief The socket to send data to the client from
-           */
-          int _skt{0};
-};
+    /** \brief A container to store client information
+     *
+     * Members:
+     *  * `short` sin_family
+     *  * `unsigned short` sin_port
+     *  * `struct` in_addr sin_addr
+     *  * `char` sin_zero[0]
+     *
+     * Needed include: `<netinet/in.h>`
+     */
+    struct sockaddr_in _cliAddr;
+
+    /** \brief A container to store server information
+     *
+     * Members:
+     *  * `short` sin_family
+     *  * `unsigned short` sin_port
+     *  * `struct` in_addr sin_addr
+     *  * `char` sin_zero[0]
+     *
+     * Needed include: `<netinet/in.h>`
+     */
+    struct sockaddr_in _servAddr;
+    /** \brief The socket to send data to the client from
+    */
+    int _skt{0};
+    int _listen_socket{0};
+}
+
+
+//template<>
+//class sender<UDP>
+//{
+/**
+ * This function initializes the parameters needed for establishing a **UDP** connection with the receiver
+ * It takes a port (`int`) and an IP Adress(`std::string`) as Arguments and
+ * only needs to be called once
+ *
+ * Modifying:
+ *    * `_cliAddr`
+ *    * `_skt`
+ *    * `port`
+ *    * `ip`
+ */
+//    void init_sender_udp(const int port, const std::string ip) {
+//        _skt = socket(AF_INET, SOCK_DGRAM, 0);
+//        _cliAddr.sin_family = AF_INET;
+//        _cliAddr.sin_port = htons(port);
+//        inet_pton(AF_INET, ip.c_str(), &(_cliAddr.sin_addr));
+//    }
+//
+//    /** \brief Main UDP function to send objects
+//     * 
+//     * The `object_to_send` should be of the same type and size (`buffer`) as the object on the [receiving](classconnector_1_1receiver.html) side
+//     */
+//    void send_udp(T * object_to_send, size_t buffer=2048) {
+//
+//        sendto(_skt, object_to_send, buffer, 0, (struct sockaddr *) &_cliAddr, sizeof(sockaddr_in));
+//    }
+//
+//    // member
+//    private:
+//    /** \brief A container to store client information
+//     *
+//     * Members:
+//     *  * `short` sin_family
+//     *  * `unsigned short` sin_port
+//     *  * `struct` in_addr sin_addr
+//     *  * `char` sin_zero[0]
+//     *
+//     * Needed include: `<netinet/in.h>`
+//     */
+//    struct sockaddr_in _cliAddr;
+//    /** \brief The socket to send data to the client from
+//    */
+//    int _skt{0};
+//    int listen_socket{0};
+//};
+
 } // namespace connector
 #endif // SENDER_H
